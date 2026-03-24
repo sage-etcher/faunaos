be1c7dbe83901e3c0ac23c9f26e48b5e83463149
sage-etcher <sage.signup@email.com>
2026-03-24 15:14:09 -0400

    add defines for advantage bitwise registers: ctrl, stat1, stat2, drive_ctrl

--- src/external/advantage.h
+++ src/external/advantage.h
@@ -2,6 +2,66 @@
 #ifndef ADVANTAGE_H
 #define ADVANTAGE_H
 
+/* I/O Control Register Format 0xf0 */
+#define CCMD_SHOW_SECTOR    0x00
+#define CCMD_KB_LSB         0x01
+#define CCMD_KB_MSB         0x02
+#define CCMD_KB_NI          0x03
+#define CCMD_CURSOR_LOCK    0x04
+#define CCMD_START_MOTORS   0x05
+#define CCMD_KB_NMI_P1      0x06
+#define CCMD_KB_NMI_P2      0x07
+#define CCMD_ALL_CAPS       0x07
+#define CCMD_MASK           0x07
+
+#define CTRL_ACQUIRE        0x08    /* 0 activated */
+#define CTRL_IO_RESET       0x10    /* 0 activated */
+#define CTRL_BLANK_DISPLAY  0x20
+#define CTRL_SPEAKER_DATA   0x40
+#define CTRL_ENABLE_VSYNC   0x80
+
+/* I/O Status 1 Register Format 0xe0 */
+#define STAT1_KB_INT        0x01
+#define STAT1_IO_INT        0x02
+#define STAT1_VSYNC_INT     0x04
+#define STAT1_NMASK_INT     0x08
+#define STAT1_WRITE_PROTECT 0x10
+#define STAT1_TRACK_ZERO    0x20
+#define STAT1_SECTOR_MARK   0x40
+#define STAT1_DISK_SDATA    0x80
+
+/* I/O Status 2 Register Format 0xd0 */
+#define STAT2_CMD_SECTOR    0x0f
+#define STAT2_CMD_KB_LSB    0x0f
+#define STAT2_CMD_KB_HSB    0x0f
+#define STAT2_CMD_KB_MI     0x01
+#define STAT2_CMD_CURLOCK   0x01
+#define STAT2_CMD_KB_NMI    0x01
+#define STAT2_CMD_ALL_CAPS  0x01
+#define STAT2_CMD_MASK      0x0f
+#define STAT2_KB_REPEAT     0x10
+#define STAT2_KB_C_OVERFLOW 0x20
+#define STAT2_KB_DATA_INT   0x40
+#define STAT2_CMD_ACK       0x80
+
+/* Drive Control Register Format */
+#define DCTRL_DRIVE_1       0x01
+#define DCTRL_DRIVE_2       0x02
+#define DCTRL_DRIVE_MASK    0x03
+#define DCTRL_STEP_PULSE    0x10
+#define DCTRL_STEPDIR_OUT   0x00
+#define DCTRL_STEPDIR_IN    0x20
+#define DCTRL_STEPDIR_MASK  0x20
+#define DCTRL_PRECOMP       0x20
+#define DCTRL_SIDE_0        0x00
+#define DCTRL_SIDE_1        0x40
+
+/* memory mapping page base addresses */
+#define MMU_PAGE_0_BADDR    0x0000
+#define MMU_PAGE_1_BADDR    0x4000
+#define MMU_PAGE_2_BADDR    0x8000
+#define MMU_PAGE_3_BADDR    0xc000
+
 __sfr __at(0x80) adv_disk_data;     /* io read/write data on disk */
 __sfr __at(0x81) adv_read_sync;     /* i  read sync byte from disk drive */
 __sfr __at(0x81) adv_drive_ctrl;    /*  o drive control register*/
