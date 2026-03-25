c518dc3be4431f0f6b640d129a4a4d0189bb09f8
sage-etcher <sage.signup@email.com>
2026-03-25 16:23:49 -0400

    clear screen and try to print 1 character, comment out the rest

--- src/hellorld/hellorld.c
+++ src/hellorld/hellorld.c
@@ -6,6 +6,7 @@
 void
 _entry0 (void)
 {
+    uint16_t p;
     struct pvid_data vdata = { 0 };
     pvid_cursor_t cursor = PVID_CURSOR_DEFAULT;
     vdata.pixel = PVID_STD_CHARSET,
@@ -17,15 +18,22 @@ _entry0 (void)
 
     adv_crt_scan = 0x00;
 
+    /* clear screen */
+    for (p = 0x0000; p < 0x5000; p++)
+    {
+        *(uint8_t *)p = 0x00;
+    }
+
+    /* write character */
     pvid_putchar ('H', &vdata);
-    pvid_putchar ('e', &vdata);
-    pvid_putchar ('l', &vdata);
-    pvid_putchar ('l', &vdata);
-    pvid_putchar ('o', &vdata);
-    pvid_putchar ('r', &vdata);
-    pvid_putchar ('l', &vdata);
-    pvid_putchar ('d', &vdata);
-    pvid_putchar (PVID_NEWLINE, &vdata);
+    //pvid_putchar ('e', &vdata);
+    //pvid_putchar ('l', &vdata);
+    //pvid_putchar ('l', &vdata);
+    //pvid_putchar ('o', &vdata);
+    //pvid_putchar ('r', &vdata);
+    //pvid_putchar ('l', &vdata);
+    //pvid_putchar ('d', &vdata);
+    //pvid_putchar (PVID_NEWLINE, &vdata);
 
     while (1)
     {
