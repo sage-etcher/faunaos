a7f71a7d02010d21a1c862be9d028b0349babbd2
sage-etcher <sage.signup@email.com>
2026-03-27 09:11:48 -0400

    enable full string print, and move clear to it's own function

--- src/hellorld/hellorld.c
+++ src/hellorld/hellorld.c
@@ -3,10 +3,11 @@
 #include "advantage_prom.h"
 #include "z80std.h"
 
+void clear (void);
+
 void
 _entry0 (void)
 {
-    uint16_t p;
     struct pvid_data vdata = { 0 };
     pvid_cursor_t cursor = PVID_CURSOR_DEFAULT;
     vdata.pixel = PVID_STD_CHARSET,
@@ -16,28 +17,45 @@ _entry0 (void)
     adv_mmu_slot1 = MMU_PAGE_VRAM_1;
     adv_mmu_slot2 = MMU_PAGE_PROM;
 
-    adv_crt_scan = 0x00;
-
     /* clear screen */
-    for (p = 0x0000; p < 0x5000; p++)
-    {
-        *(uint8_t *)p = 0x00;
-    }
+    adv_crt_scan = 0x00;
+    clear ();
 
     /* write character */
     pvid_putchar ('H', &vdata);
-    //pvid_putchar ('e', &vdata);
-    //pvid_putchar ('l', &vdata);
-    //pvid_putchar ('l', &vdata);
-    //pvid_putchar ('o', &vdata);
-    //pvid_putchar ('r', &vdata);
-    //pvid_putchar ('l', &vdata);
-    //pvid_putchar ('d', &vdata);
-    //pvid_putchar (PVID_NEWLINE, &vdata);
+    pvid_putchar ('e', &vdata);
+    pvid_putchar ('l', &vdata);
+    pvid_putchar ('l', &vdata);
+    pvid_putchar ('o', &vdata);
+    pvid_putchar ('r', &vdata);
+    pvid_putchar ('l', &vdata);
+    pvid_putchar ('d', &vdata);
+    pvid_putchar (PVID_NEWLINE, &vdata);
 
     while (1)
     {
         cpu_halt ();
+    }
+}
+
+void
+clear (void)
+{
+    const uint8_t MAX_X = 0x4f;
+    const uint8_t MAX_Y = 0xf0;
+
+    register uint8_t x;
+    register uint8_t y;
+    register uint8_t *ptr;
+
+
+    for (x = 0; x < MAX_X; x++)
+    {
+        for (y = 0; y < MAX_Y; y++)
+        {
+            ptr = (uint8_t *)(((uint16_t)x << 8) | y);
+            *ptr = 0x00;
+        }
     }
 }
 
