b04b3da50b90de1662c39cd05b62c047acbe2edb
sage-etcher <sage.signup@email.com>
2026-03-27 13:24:15 -0400

    wrap pvid_putchar to protect IX (sdcc frame pointer). also add comment documentation

--- src/external/advantage_prom.asm
+++ src/external/advantage_prom.asm
@@ -10,9 +10,19 @@
 ;------------------------------------------------------------------------
     .area _CODE
 
+;modifies BC, HL
 ; A: char c
 ;DE: struct pvid_data *vdata
 _pvid_putchar:
+    push ix                     ;protect SDCC frame pointer
+    call _pvid_putchar_core     ;modifies IX
+    pop ix                      ;restore IX
+    ret
+
+;modifies BC, HL, IX
+; A: char c
+;DE: struct pvid_data *vdata
+_pvid_putchar_core:
     ld ix,#0        ;ix = vdata
     add ix,de
     pop de          ;de = return_address
