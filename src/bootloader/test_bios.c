32c8d422ad6a46452a33b8d44a47672b4162b42c
sage-etcher <sage.signup@email.com>
2026-06-05 16:28:43 -0400

    start adding test_blk_read() to the test suite

--- src/bootloader/test_bios.c
+++ src/bootloader/test_bios.c
@@ -140,6 +140,70 @@ test_blk_reset(void)
     T_RETURN(T_PASS, NULL);
 }
 
+static uint8_t
+test_blk_read(void)
+{
+    enum {
+        BLOCK_CNT = 3,
+        BUF_SIZE = 512 * BLOCK_CNT,
+    };
+    static uint8_t buf[BUF_SIZE];
+
+    REQUIRE(s_blk_set_drive0);
+    REQUIRE(s_blk_set_platter0);
+    REQUIRE(s_blk_set_cylinder0);
+    REQUIRE(s_blk_set_sector0);
+    REQUIRE(s_blk_reset);
+
+    blk_reset();
+    t_assert_eq(BLKERR_NULL_DEREF, blk_read(1, &buf));
+
+    /* A top 00:0 - A top 00:3 */
+    (void)blk_set_drive(0);
+    (void)blk_set_platter(0);
+    (void)blk_set_cylinder(0);
+    (void)blk_set_sector(0);
+    (void)memset(&buf, 0x00, BUF_SIZE);
+    t_assert_eq(BLKERR_OK, blk_read(BLOCK_CNT, &buf));
+    t_assert_mem_eq(&buf, RAND_SECDATA, BUF_SIZE);
+
+    t_assert_eq(BLKERR_NULL_DEREF, blk_read(1, NULL));
+    t_assert_eq(BLKERR_OK, blk_read(0, NULL));
+    t_assert_eq(BLKERR_OK, blk_read(0, &buf));
+
+    /* A top 33:9 - A top 34:1 */
+    (void)blk_set_cylinder(33);
+    (void)blk_set_sector(9);
+    (void)memset(&buf, 0x00, BUF_SIZE);
+    t_assert_eq(BLKERR_OK, blk_read(BLOCK_CNT, &buf));
+    t_assert_mem_eq(&buf, RAND_SECDATA, BUF_SIZE);
+
+    /* A top 34:9 - A bottom 34:1 */
+    (void)blk_set_sector(9);
+    (void)memset(&buf, 0x00, BUF_SIZE);
+    t_assert_eq(BLKERR_OK, blk_read(BLOCK_CNT, &buf));
+    t_assert_mem_eq(&buf, RAND_SECDATA, BUF_SIZE);
+
+    /* A top 33:9 - A top 34:1 */
+    (void)blk_set_cylinder(33);
+    (void)blk_set_sector(9);
+    (void)memset(&buf, 0x00, BUF_SIZE);
+    t_assert_eq(BLKERR_OK, blk_read(BLOCK_CNT, &buf));
+    t_assert_mem_eq(&buf, RAND_SECDATA, BUF_SIZE);
+
+    T_RETURN(T_PASS, NULL);
+}
+
+static uint8_t
+test_blk_write(void)
+{
+}
+
+static uint8_t
+test_blk_write_protect(void)
+{
+}
+
 
 DEFINE_RUN_ALL_TESTS(
     test_blk_unset_drive,
